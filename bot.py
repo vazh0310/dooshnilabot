2b6cf700bd1267092f36d4d00d5a8afb9a98bc69
vazh0310 <60910587+vazh0310@users.noreply.github.com>
2021-10-23 19:05:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,59 +1,74 @@
 from random import choice
 import discord
+import config
 
 client = discord.Client()
 
-words = [
-    "блять", "ты че долбоеб?", "ору", "иди нахуй", "ебанат", "еблан",
-    "даун", "пиздец", "придурок", "уебан", "убейся об стенку аутист",
-    "еблан тупорылый", "ебало представили?", "в чем я не прав?",
-    "негры заебали", "за россию, сука!", "ты мудак или да?",
-    "ой блядь началось...", "хуй соси", "пидор штоль?", "порвался?",
-    "у тебя НЕ бомбит, верю", "говорят ты хуи сосешь, это правда?",
-    "дегенерат", "хуйло", "ебаный козел", "черножопый даун", "конченый",
-    "это мне?", "ебало завали", "оффнись нахуй, чмо"
-]
+def load_words():
+    global words
+    words = []
+    
+    try:
+        f = open(config.wordsfile, 'r')
 
-very_dumb_words = [
-    "я тебе все сказал", "повторять не собираюсь", "собака, собачья жизнь!",
-    "тебя не спрашивали", "я больше не повторяю", "все детали были обговорены",
-    "не надо тут ля-ля, уважаемый", "ну-ну, маня", "так сказать то что хотел?",
-    "уведомляю вас о том, что я выставил вам требования", "порвался таки",
-    "при выполнении указанных требований немецкое командование гарантирует вам \
-    исключение из списка долбоебов"
-]
+        ln = 0
+        for line in f:
+            words.append(line)
+            ln += 1
 
-admin_words = [
-    "может мне еще и отсосать у тебя?", "бля, может сам офнешься?",
-    "нахуй не здесь", "это мне?", "отъебись", "я за россию, а ты вижу пидераст!"
-]
+        print(f"Loaded {ln} phrases")
+    except FileNotFoundError:
+        print(f"ERROR: File '{config.wordsfile}' not found!")
+    finally:
+        f.close()
 
-premsg = "Мне задали оскорблять тебя: "
+def load_admwords():
+    global admin_words
+    admin_words = []
+    
+    try:
+        f = open(config.admwordsfile, 'r')
 
-idiots_id = [
+        ln = 0
+        for line in f:
+            admin_words.append(line)
+            ln += 1
 
-]
+        print(f"Loaded {ln} admin phrases")
+    except FileNotFoundError:
+        print(f"ERROR: File '{config.admwordsfile}' not found!")
+    finally:
+        f.close()
+            
+def load_dumbwords():
+    global very_dumb_words
+    very_dumb_words = []
+    
+    try:
+        f = open(config.dumbwordsfile, 'r')
 
-very_stupid_id = [
+        ln = 0
+        for line in f:
+            very_dumb_words.append(line)
+            ln += 1
 
-]
-
-admins = [
-    557251333250351142
-]
-
+        print(f"Loaded {ln} dumb phrases")
+    except FileNotFoundError:
+        print(f"ERROR: File '{config.dumbwordsfile}' not found!")
+    finally:
+        f.close()
+        
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
-    if '.dooshnila_exit' in message.content:
-        if message.author.id in admins:
-            #await message.channel.send("Ладно.")
+    if ".dooshnila_exit" in message.content:
+        if message.author.id in config.admins:
             embed = discord.Embed(title="Ладно.")
             await message.channel.send(content=None, embed=embed)
-            exit()
+            exit(0)
         else:
             word = choice(admin_words)
             await message.channel.send(content=word)
@@ -62,9 +77,8 @@ async def on_message(message):
         await message.channel.send(content=word)
     if message.author.id in idiots_id or message.content == ".dooshnila":
         word = choice(words)
-        #await message.channel.send(f"{premsg} {word}")
         embed = discord.Embed(title="Режим душнилы активирован", description=word)
         await message.channel.send(content=None, embed=embed)
 
 if __name__ == '__main__':
-    client.run(token, bot=False)
+    client.run(config.token, bot=config.is_bot)
